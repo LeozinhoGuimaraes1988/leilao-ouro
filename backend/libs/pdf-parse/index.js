d24b71287155b48ce0104e9e5f5b636ca1a3aa73
Leonardo Guimarães <leosinhoguimaraes@hotmail.com>
2025-06-27 12:15:16 -0300

    Criação de lib interna

--- backend/libs/pdf-parse/index.js
+++ backend/libs/pdf-parse/index.js
@@ -1,5 +1,4 @@
-import pdfParse from 'pdf-parse';
-
+import pdfParse from 'pdf-parse/lib/pdf-parse.js';
 export default async function extrairTextoPdf(buffer) {
   const result = await pdfParse(buffer);
   return result.text;
